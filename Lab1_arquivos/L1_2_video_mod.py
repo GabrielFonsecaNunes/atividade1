7599ab3234c6eb47a442756437139d6527f8f3e2
GabrielFonsecaNunes <fonseca.nunes1996@gmail.com>
2023-06-23 07:21:57 -0300

    Update Files

---
+++ Lab1_arquivos/L1_2_video_mod.py
@@ -0,0 +1,31 @@
+import numpy as np
+import cv2 as cv
+
+cap = cv.VideoCapture(0)
+
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
+    
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    # if frame is read correctly ret is True
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+    
+    # Display the resulting frame
+    cv.imshow('frame', frame)
+    
+    key = cv.waitKey(1)
+    if key == ord('q'):
+        break
+    elif key == ord('x'):
+        cv.imwrite('foto1.png', frame)
+        print("Foto salva como 'foto1.png'")
+        break
+
+# When everything done, release the capture
+cap.release()
+cv.destroyAllWindows()
---
+++ Lab1_arquivos/L1_3_webcam_mod.py
@@ -0,0 +1,36 @@
+import numpy as np
+import cv2 as cv
+
+cap = cv.VideoCapture(0)
+
+# Get current width of frame
+width = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
+# Get current height of frame
+height = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
+# Define Video Frame Rate in fps
+fps = 25.0
+
+# Define the codec and create VideoWriter object
+fourcc = cv.VideoWriter_fourcc(*'XVID')
+out = cv.VideoWriter('saida.avi', fourcc, fps, (width, height))
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+    
+    # write the frame
+    out.write(frame)
+    
+    # Display the frame
+    cv.imshow('frame', frame)
+    
+    # wait for 'q' key to exit
+    if cv.waitKey(1) == ord('q'):
+        break
+
+# Release everything if job is finished
+cap.release()
+out.release()
+cv.destroyAllWindows()
